c428c9324f10251dc221ceba97d480f7a5e818fb
xiaolong70701 <xiaolong70701@gmail.com>
2022-04-28 02:25:43 +0800

    WIP

--- api/test.py
+++ api/test.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import time
 
+def get_data(yymmdd):
+    ## set the url
+    url = f'https://www.twse.com.tw/fund/BFI82U?response=html&dayDate=' + yymmdd
+    ## save the results into dataframe
+    raw = pd.read_html(url, encoding='utf-8',header=0)[0]
+
+    ## get the amount
+    amount = raw.iloc[1:5, 3].tolist()
+    return amount    
+
 ## Set the query
 years = ['2017', '2018', '2019', '2020', '2021']
 months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
